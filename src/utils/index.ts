3dde6e5eca6e8b492c4e56dc4f14bf0cb5b118d3
zhu-hong <zhxhyy@qq.com>
2022-01-10 20:19:26 +0800

    unknown

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,51 @@
 export const renderText = (node: HTMLElement, text: string) => {
-  const maxLen = node.getAttribute('data-len')
-  const len = text.slice(0, Number(maxLen)).length
+  let maxLen = Number(node.getAttribute('data-len'))
+  let maxLenFreeze = Number(node.getAttribute('data-len'))
+  let len = text.slice(0, 20).length
+
+  let trueLen = 0
+  for(let i = 0; i < len; i++) {
+    if(maxLen >= 1) {
+      if(text.slice(0, len).charCodeAt(i) > 255) {
+        maxLen-=1
+      } else {
+        maxLen-=0.5
+      }
+      trueLen+=1
+    }
+  }
+
+  let mergeLen = 0
+  for(let i = 0; i < trueLen; i++) {
+    if(text.slice(0, trueLen).charCodeAt(i) > 255) {
+      mergeLen+=1
+    } else {
+      mergeLen+=0.5
+    }
+  }
   
   const isCenter = node.getAttribute('data-center')
   if(isCenter) {
-    const basic = node.getAttribute('data-basic')
-    const span = node.getAttribute('data-span')
+    const basic = Number(node.getAttribute('data-basic'))
+    const span = Number(node.getAttribute('data-span'))
+    
+    // 中文字符数
+    let CHNCount = 0
+    for(let i = 0; i < trueLen; i++) {
+      if(text.slice(0, trueLen).charCodeAt(i) > 255) {
+        CHNCount++
+      }
+    }
 
-    const x = Number(basic) - Number(span) * len
+    const x = basic - span * CHNCount - span / 2 * (trueLen - CHNCount)
 
     node.setAttribute('x', String(x))
   }
   
-  node.textContent = text.slice(0, len)
+  if(mergeLen >= maxLenFreeze - 0.5 || len === 20) {
+    console.log(1)
+    node.textContent = text.slice(0, trueLen - 1) + '...'
+    return
+  }
+  node.textContent = text.slice(0, trueLen)
 }
