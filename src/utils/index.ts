3ee3acc2198698eff71e2c9c23a21cf206444be8
zhu-hong <zhxhyy@qq.com>
2022-01-23 17:31:10 +0800

    svg2base64

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -15,3 +15,34 @@ export const renderText = (node: HTMLElement, text: string, wrapperWidth: number
     node.textContent += '...'
   }
 }
+
+export  function encodeSvg(svgString) {
+  return svgString.replace('<svg', (~svgString.indexOf('xmlns') ? '<svg' : '<svg xmlns="http://www.w3.org/2000/svg"'))
+    //
+    //   Encode (may need a few extra replacements)
+    //
+    .replace(/"/g, '\'')
+    .replace(/%/g, '%25')
+    .replace(/#/g, '%23')
+    .replace(/{/g, '%7B')
+    .replace(/}/g, '%7D')
+    .replace(/</g, '%3C')
+    .replace(/>/g, '%3E')
+    .replace(/\s+/g, ' ')
+
+    // 
+    //    The maybe list (add on documented fail)
+    // 
+    //  .replace(/&/g, '%26')
+    //  .replace('|', '%7C')
+    //  .replace('[', '%5B')
+    //  .replace(']', '%5D')
+    //  .replace('^', '%5E')
+    //  .replace('`', '%60')
+    //  .replace(';', '%3B')
+    //  .replace('?', '%3F')
+    //  .replace(':', '%3A')
+    //  .replace('@', '%40')
+    //  .replace('=', '%3D')
+    ;
+}
